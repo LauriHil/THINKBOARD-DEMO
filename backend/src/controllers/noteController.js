828aeffd1c51f6eafd3d6fb26511b7af602bdffd
Lauri <lauri.hiltunen@outlook.com>
2025-11-22 16:53:15 +0200

    Implemented ratelimiting middleware with upstash and updated the app start-up logic to only start after db is connected.

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -1,8 +1,8 @@
 import Note from "../models/Note.js"
 
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
     try {
-        const notes = await Note.find(); 
+        const notes = await Note.find().sort({ createdAt: -1 }); // sorting so that, the most recent one is first
         res.status(200).json(notes); 
     } catch (error) {
         console.error("Error in getAllNotes controller", error);    //Postman or other means of testing the HTTP METHODS
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,22 +2,32 @@ import express from "express"
 import noteRoutes from "./routes/noteRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-connectDB();
+
 
 //Middleware
-app.use(express.json());
+app.use(express.json()); //middleware parses JSON bodies: req.body
+
+//Small custom middleware
+app.use((req, res, next) => {
+    console.log(`We just got a new request. Req method is ${req.method} & Req URL is ${req.url}`);
+    next();
+})
+
+//using ratelimiter middleware
+app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes);
 
-app.listen(PORT, () => {
+connectDB().then(() => {
+    app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
     console.log("localhost:", PORT);
 });
-
-//1h 9s videolla
+})
